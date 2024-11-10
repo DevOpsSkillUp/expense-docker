fb5abdbee960ff682a373dbfaf1eb288d25d3aeb
medurubharath <medurubharathstvd@gmail.com>
2024-11-09 19:22:03 -0600

    scripts

---
+++ Install-docker.sh
@@ -0,0 +1,16 @@
+#! /bin/bas
+
+
+#check wether root user or not will do later
+
+R="\e[31m"
+N="\e[0m"
+
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
+echo -e "$R logout and login again $N"
